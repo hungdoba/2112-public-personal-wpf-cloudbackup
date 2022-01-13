7eac337bc78e5a0dd4224fd0083f831ec72ec240
Jeiv <hungdoba.hdb@gmail.com>
2022-01-13 10:39:04 +0900

    auto hide when start

--- AutoBackUpToCloud/WindowSettup.xaml.cs
+++ AutoBackUpToCloud/WindowSettup.xaml.cs
@@ -104,6 +104,7 @@ namespace AutoBackUpToCloud
             if (isAppRunning == true)
             {
                 startProgram();
+                WindowState = WindowState.Minimized;
             }
             else if(isAppRunning == false)
             {
